95cd789a71c942167b4c78660f71c3ac5a6fae6d
BrenoReboredo-09 <127802083+BrenoReboredo-09@users.noreply.github.com>
2024-03-13 09:55:01 -0300

    Add files via upload

---
+++ atividade1/VariaveisOperadores.js
@@ -0,0 +1,12 @@
+let num1 = 5
+let num2 = 3
+
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let div = num1/num2
+
+console.log(`resultado da soma entre ${num1} e ${num2} é ${soma}`)
+console.log(`resultado da subtração entre ${num1} e ${num2} é ${sub}`)
+console.log(`resultado da multiplicação entre ${num1} e ${num2} é ${mult}`)
+console.log(`resultado da divisão entre ${num1} e ${num2} é ${div}`)
